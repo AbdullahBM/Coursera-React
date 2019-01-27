1c7c09d5affbda1a9a413f5103a56762e1f5eb19
Abdulla Bin Mustaqeem <abdullah.m9404@outlook.com>
2019-01-28 00:35:06 +0500

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
 // import logo from "./logo.svg";
+import { DISHES } from "./shared/dishes";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    dishes: DISHES
+  };
+
   render() {
     return (
       <div>
@@ -13,7 +18,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
