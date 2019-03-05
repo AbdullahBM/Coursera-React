a203adf68ad6b811e1b08f5fb19b8c06fa111332
Abdulla Bin Mustaqeem <abdullah.m9404@outlook.com>
2019-03-05 22:18:07 +0500

    make build and putits file to server folder

---
+++ Con Fusion/build/precache-manifest.2aaac0a2a5f2884fb9499eccda59954c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "eb2e59dc4909334e13fe",
+    "url": "/static/css/main.1775845b.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "74865b63e735b308e87a",
+    "url": "/static/js/2.74865b63.chunk.js"
+  },
+  {
+    "revision": "eb2e59dc4909334e13fe",
+    "url": "/static/js/main.eb2e59dc.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "74865b63e735b308e87a",
+    "url": "/static/css/2.2518f089.chunk.css"
+  },
+  {
+    "revision": "6cef53be2f8aa6151ebacc34b75ec32b",
+    "url": "/index.html"
+  }
+];
